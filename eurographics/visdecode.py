9b7f5e353c818932a00d08a3218ce457f1784b12
martinsinnona <martinsinnona@outlook.es>
2024-09-11 18:44:34 -0300

    added new metrics

--- eurographics/visdecode.py
+++ eurographics/visdecode.py
@@ -1,22 +1,27 @@
 from transformers import AutoProcessor, Pix2StructForConditionalGeneration
 from huggingface_hub import snapshot_download
+import os
+
+MAX_LENGTH = 600
 
 def load_model(owner, model_name, device):
     
-    path = "/mnt/disk2/msinnona/models/" + model_name
+    path = os.getcwd() + "/models/"
 
     processor = None
     model = None
 
     try:
-        processor = AutoProcessor.from_pretrained("google/matcha-base")
-        model = Pix2StructForConditionalGeneration.from_pretrained(path).to(device)
+        model = Pix2StructForConditionalGeneration.from_pretrained(path + model_name).to(device)
     except:
+        snapshot_download(repo_id = owner + "/" + model_name, local_dir = path + model_name)
+        model = Pix2StructForConditionalGeneration.from_pretrained(path + model_name)
 
-        snapshot_download(repo_id = owner + "/" + model_name, local_dir=path)
-
-        processor = AutoProcessor.from_pretrained("google/matcha-base")
-        model = Pix2StructForConditionalGeneration.from_pretrained(path)
+    try:
+        processor = AutoProcessor.from_pretrained(path + "matcha-base")
+    except:
+        snapshot_download(repo_id = "google/matcha-base", local_dir = path + "matcha-base")
+        processor = AutoProcessor.from_pretrained(path + "matcha-base")
 
     processor.image_processor.is_vqa = False
 
@@ -27,12 +32,15 @@ def generate(processor, model, image, device):
     model.eval()
     inputs = processor(images = image, return_tensors = "pt", max_patches = 1024).to(device)
 
-    tokens = model.generate(flattened_patches = inputs.flattened_patches, attention_mask = inputs.attention_mask, max_length = 600)
+    tokens = model.generate(flattened_patches = inputs.flattened_patches, attention_mask = inputs.attention_mask, max_length = MAX_LENGTH)
     output = processor.batch_decode(tokens, skip_special_tokens = True)[0]
 
     return output
 
-def eval_model(processor, model, image, device):
+def eval_model(processor, model, images, texts_gt, device):
 
-    output = generate(processor, model, image, device)
-    return output
+    outputs = [generate(processor, model, image, device) for image in images]
+
+    
+
+    return outputs[0]
